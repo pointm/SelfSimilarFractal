d40ebb96c0d00c86810f6694714aab96177ae3c6
pointm <2765211704@qq.com>
2023-09-20 17:40:52 +0800

    一个尝试

--- testslice.py
+++ testslice.py
@@ -461,3 +461,35 @@ class GradientImageFromArray(Scene):
         image = ImageMobject(imageArray).scale(2)
         image.background_rectangle = SurroundingRectangle(image, GREEN)
         self.add(image, image.background_rectangle)
+
+from manim import *
+
+class ImageInterpolationEx(Scene):
+    def construct(self):
+        img = ImageMobject(np.uint8([[63, 0, 0, 0],
+                                        [0, 127, 0, 0],
+                                        [0, 0, 191, 0],
+                                        [0, 0, 0, 255]
+                                        ]))
+
+        img.height = 2
+        img1 = img.copy()
+        img2 = img.copy()
+        img3 = img.copy()
+        img4 = img.copy()
+        img5 = img.copy()
+
+        img1.set_resampling_algorithm(RESAMPLING_ALGORITHMS["nearest"])
+        img2.set_resampling_algorithm(RESAMPLING_ALGORITHMS["lanczos"])
+        img3.set_resampling_algorithm(RESAMPLING_ALGORITHMS["linear"])
+        img4.set_resampling_algorithm(RESAMPLING_ALGORITHMS["cubic"])
+        img5.set_resampling_algorithm(RESAMPLING_ALGORITHMS["box"])
+        img1.add(Text("nearest").scale(0.5).next_to(img1,UP))
+        img2.add(Text("lanczos").scale(0.5).next_to(img2,UP))
+        img3.add(Text("linear").scale(0.5).next_to(img3,UP))
+        img4.add(Text("cubic").scale(0.5).next_to(img4,UP))
+        img5.add(Text("box").scale(0.5).next_to(img5,UP))
+
+        x= Group(img1,img2,img3,img4,img5)
+        x.arrange()
+        self.add(x)
